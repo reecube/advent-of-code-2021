c9f3b90f82c54ea717bb52be7b64444ef9c947fc
Yves Riedener <git@reecube.com>
2021-12-19 14:32:30 +0100

    convert day 9

---
+++ src/solutions/Day091.ts
@@ -0,0 +1,59 @@
+import { Solution } from '../model/Solution';
+import { Test } from '../model/Test';
+import { Result } from '../model/Result';
+
+export default class DefaultSolution implements Solution<number[][]> {
+  readonly tests: Test[] = [
+    // Implemented without TDD
+  ];
+
+  parse(input: string): number[][] {
+    return input.split('\n').filter(it => it.length).map(line => {
+      return line.split('').map(it => parseInt(it));
+    });
+  }
+
+  solve(input: number[][]): Result {
+    const size = input.length;
+
+    let result = 0;
+
+    for (let y = 0; y < size; y++) {
+      for (let x = 0; x < size; x++) {
+        let counterLower = 0;
+
+        for (let y2 = -1; y2 <= 1; y2++) {
+          const relY = y + y2;
+
+          const absY2 = Math.abs(y2);
+
+          for (let x2 = -1; x2 <= 1; x2++) {
+            const relX = x + x2;
+
+            // Skip diagonal ones
+            if (absY2 === Math.abs(x2)) continue;
+
+            if (relY < 0 || relY >= size || relX < 0 || relX >= size) {
+              counterLower += 1;
+              continue;
+            }
+
+            const diff = input[y][x] - input[relY][relX];
+
+            if (diff >= 0) continue;
+
+            counterLower += 1;
+          }
+        }
+
+        if (counterLower < 4) continue;
+
+        const riskLevel = 1 + input[y][x];
+
+        result += riskLevel;
+      }
+    }
+
+    return result;
+  }
+}
